e3ffbfbcbe080955ec52105fce1c461c9c56ee38
MarieAmbrazhei <marykapuza@gmail.com>
2024-05-12 20:32:04 +0300

    fixed module name homework#5.py, fixed constant name, added docstring

--- homework_5.py
+++ homework_5.py
@@ -1,14 +1,15 @@
+"""Main module"""
 # Заменить символ “#” на символ “/” в строке 'www.my_site.com#about'
-str_to_update = 'www.my_site.com#about'
-print(str_to_update.replace('#', "/"))
+STR_TO_UPDATE = 'www.my_site.com#about'
+print(STR_TO_UPDATE.replace('#', "/"))
 # Напишите программу, которая добавляет ‘ing’ к словам
 given_words = ["do", "go", "join"]
-ing_addition = "ing"
+ING_ADDITION = "ing"
 for word in given_words:
-    print(word + ing_addition)
+    print(word + ING_ADDITION)
 # В строке “Ivanou Ivan” поменяйте местами слова => "Ivan Ivanou"
-name_to_update = 'Ivanou Ivan'
-print(' '.join(name_to_update.split()[::-1]))
+NAME_TO_UPDATE = 'Ivanou Ivan'
+print(' '.join(NAME_TO_UPDATE.split()[::-1]))
 # Напишите программу которая удаляет пробел в начале, в конце строки
 words_with_spaces = ['    spaces_before_word', 'spaces_after_word    ']
 print([word.strip() for word in words_with_spaces])
